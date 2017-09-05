10cde479d8924104912f436245bda35001b3213a
Florian Baumgärtel <flo.baumi87@gmail.com>
2017-09-05 12:04:57 +0200

    popu

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require("path");
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+
+
+
+var config = {
+  entry: SRC_DIR + "/app/index.js",
+  output: {
+    path: DIST_DIR + "/app",
+    filename: "bundle.js",
+    publicPath: "/app/"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js|.jsx?$/,
+        include: SRC_DIR,
+        loader: "babel-loader",
+
+        exclude: [nodeModulesPath],
+        query: {
+          presets: ["react", "es2015","stage-2"]
+        }
+      },
+
+      {
+        test: /\.css$/,
+        include: SRC_DIR,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+
+    ]
+  }
+
+
+};
+
+module.exports = config;
